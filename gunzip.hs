b9b0624a772433d8da1f294bf11f3ae9e43df91a
Daria Jung <djj2115cu@gmail.com>
2014-07-15 15:02:33 -0400

    add checks for flags

--- gunzip.hs
+++ gunzip.hs
@@ -4,6 +4,7 @@ import Data.BitArray
 import Data.Word
 import Data.Char
 import Text.Printf (printf)
+import Data.Binary as BN
 
 type GZipFlags = Word8
 
@@ -12,23 +13,12 @@ data BitStream = BitStream {
     bv :: BitArray
 }
 
-{-      
-1 byte  flags
-        bit 0 set: file probably ascii text
-        bit 1 set: continuation of multi-part gzip file
-        bit 2 set: extra field present
-        bit 3 set: original file name present
-        bit 4 set: file comment present
-        bit 5 set: file is encrypted
-        bit 6,7:   reserved
--}
-
 -- total of 10 bytes
 data GZipHeader = GZipHeader {
     magicWords :: [Word8], -- length 2
     compressionMethod :: Word8,
     flags :: GZipFlags,
-    mtime :: [Word8], -- length 4
+    mtime :: [Word8], -- file modification time in Unix format, length 4
     extraFlags :: Word8,
     os :: Word8
 } deriving (Show)
@@ -52,6 +42,11 @@ data HuffmanHeader = HuffmanHeader {
 }
 
 -- translate bytes to binary
+toBool (x:xs)
+    | x == 1    = True : toBool xs
+    | x == 0    = False : toBool xs
+toBool [] = []
+
 toBinary 0 = [0, 0, 0, 0, 0, 0, 0, 0]
 toBinary n = ensureEight $ reverse (helper n)
 
@@ -69,13 +64,19 @@ toHex bytes = C.unpack bytes >>= printf "%02x"
 main = do
     contents <- B.readFile "keats.txt.gz"
     let uint8 = B.unpack contents
-    --print $ take 2 uint8
         header = GZipHeader {
                 magicWords = take 2 uint8,
-                compressionMethod = uint8 !! 3,
-                flags = uint8 !! 4,
+                compressionMethod = uint8 !! 2,
+                flags = uint8 !! 3,
                 mtime = take 4 $ drop 4 uint8,
                 extraFlags = uint8 !! 8,
                 os = uint8 !! 9
             }
     return header
+
+-- Checks for various flags
+hasAscii flag = ((toBool $ toBinary $ fromIntegral flag) !! 7) && True
+hasCont flag =  ((toBool $ toBinary $ fromIntegral flag) !! 6) && True
+hasExtra flag = ((toBool $ toBinary $ fromIntegral flag) !! 5) && True
+hasName flag =  ((toBool $ toBinary $ fromIntegral flag) !! 4) && True
+hasComment flag = ((toBool $ toBinary $ fromIntegral flag) !! 3) && True
