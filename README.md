67f54e0cc4fba371b7a0d77cf2e281e548a1f1b5
Daria Jung <djj2115cu@gmail.com>
2014-07-15 12:28:46 -0400

    update readme, get gzipheader data

--- README.md
+++ README.md
@@ -3,6 +3,46 @@ gunzip
 
 An implementation of gunzip/gzip -d in Haskell. 
 
+Notes 
+----
+
+Byte Flags:
+
+```
+bit 0 set: file probably ascii text
+bit 1 set: continuation of multi-part gzip file
+bit 2 set: extra field present
+bit 3 set: original file name present
+bit 4 set: file comment present
+bit 5 set: file is encrypted
+bit 6,7:   reserved
+```
+
+Operating System Codes:
+
+```
+0 - FAT file system (DOS, OS/2, NT) + PKZIPW 2.50 VFAT, NTFS
+1 - Amiga
+2 - VMS (VAX or Alpha AXP)
+3 - Unix
+4 - VM/CMS
+5 - Atari
+6 - HPFS file system (OS/2, NT 3.x)
+7 - Macintosh
+8 - Z-System
+9 - CP/M
+10 - TOPS-20
+11 - NTFS file system (NT)
+12 - SMS/QDOS
+13 - Acorn RISC OS
+14 - VFAT file system (Win95, NT)
+15 - MVS (code also taken for PRIMOS)
+16 - BeOS (BeBox or PowerMac)
+17 - Tandem/NSK
+18 - THEOS
+```
+
+
 Resources 
 ----
 
--- gunzip.hs
+++ gunzip.hs
@@ -3,20 +3,35 @@ import qualified Data.ByteString.Char8 as C
 import Data.BitArray
 import Data.Word
 import Data.Char
+import Text.Printf (printf)
+
+type GZipFlags = Word8
 
 data BitStream = BitStream {
     stream :: IO (),
     bv :: BitArray
 }
 
+{-      
+1 byte  flags
+        bit 0 set: file probably ascii text
+        bit 1 set: continuation of multi-part gzip file
+        bit 2 set: extra field present
+        bit 3 set: original file name present
+        bit 4 set: file comment present
+        bit 5 set: file is encrypted
+        bit 6,7:   reserved
+-}
+
+-- total of 10 bytes
 data GZipHeader = GZipHeader {
-    id :: [Word8], -- length 2
+    magicWords :: [Word8], -- length 2
     compressionMethod :: Word8,
     flags :: GZipFlags,
     mtime :: [Word8], -- length 4
     extraFlags :: Word8,
     os :: Word8
-}
+} deriving (Show)
 
 data GZipMetadata = GZipMetadata {
     header :: GZipHeader,
@@ -25,15 +40,42 @@ data GZipMetadata = GZipMetadata {
     fname :: [Char],
     fcomment :: [Char],
     crc16 :: Word16
-}
+} deriving (Show)
 
 data BlockFormat = BlockFormat {
     last :: Bool,
-    blockType :: BitArray
+    blockType :: BitArray -- length 2
+} deriving (Show)
+
+data HuffmanHeader = HuffmanHeader {
+    
 }
 
-type GZipFlags = Word8
+-- translate bytes to binary
+toBinary 0 = [0, 0, 0, 0, 0, 0, 0, 0]
+toBinary n = ensureEight $ reverse (helper n)
+
+helper 0 = []
+helper n | n `mod` 2 == 1 = 1 : helper (n `div` 2)
+         | n `mod` 2 == 0 = 0 : helper (n `div` 2)
+
+ensureEight n = (replicate (8 - length n) 0) ++ n
+
+toHex :: B.ByteString -> String
+toHex bytes = C.unpack bytes >>= printf "%02x"
+
+-- Magic numbers are: 0x1f, 0x8b, or 31 and 139
 
 main = do
     contents <- B.readFile "keats.txt.gz"
-    print contents
+    let uint8 = B.unpack contents
+    --print $ take 2 uint8
+        header = GZipHeader {
+                magicWords = take 2 uint8,
+                compressionMethod = uint8 !! 3,
+                flags = uint8 !! 4,
+                mtime = take 4 $ drop 4 uint8,
+                extraFlags = uint8 !! 8,
+                os = uint8 !! 9
+            }
+    return header
