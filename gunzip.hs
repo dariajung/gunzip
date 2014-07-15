af928b74cb434b2cdd00cea6c4300ea5f7230bcf
Daria Jung <djj2115cu@gmail.com>
2014-07-15 11:34:55 -0400

    set up more data types

--- gunzip.hs
+++ gunzip.hs
@@ -1,16 +1,38 @@
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as C
+import Data.BitArray
+import Data.Word
+import Data.Char
 
-data Header = Header {
-    id :: B.ByteString,
-    compressionMethod :: B.ByteString
-    flags :: GZipFlags,
-    mtime :: B.ByteString,
-    extraFlags :: B.ByteString,
-    os :: B.ByteString
+data BitStream = BitStream {
+    stream :: IO (),
+    bv :: BitArray
 }
 
-type GZipFlags = B.ByteString
+data GZipHeader = GZipHeader {
+    id :: [Word8], -- length 2
+    compressionMethod :: Word8,
+    flags :: GZipFlags,
+    mtime :: [Word8], -- length 4
+    extraFlags :: Word8,
+    os :: Word8
+}
+
+data GZipMetadata = GZipMetadata {
+    header :: GZipHeader,
+    xlen :: Word16,
+    extra :: [Char],
+    fname :: [Char],
+    fcomment :: [Char],
+    crc16 :: Word16
+}
+
+data BlockFormat = BlockFormat {
+    last :: Bool,
+    blockType :: BitArray
+}
+
+type GZipFlags = Word8
 
 main = do
     contents <- B.readFile "keats.txt.gz"
